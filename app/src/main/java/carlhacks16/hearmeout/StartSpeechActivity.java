0f9decf1fb43c4e862af40630a982a021c0db644
YifanGong <ygong@macalester.edu>
2016-04-09 16:02:27 -0500

    added visualizer

--- app/src/main/java/carlhacks16/hearmeout/StartSpeechActivity.java
+++ app/src/main/java/carlhacks16/hearmeout/StartSpeechActivity.java
@@ -1,102 +1,202 @@
 package carlhacks16.hearmeout;
 
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.content.pm.PackageManager;
+import android.app.Activity;
+import android.media.AudioFormat;
+import android.media.AudioRecord;
 import android.media.MediaRecorder;
+import android.os.Bundle;
 import android.os.Environment;
-import android.widget.Button;
+import android.text.format.Time;
 import android.view.View;
-import android.media.MediaPlayer;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
-public class StartSpeechActivity extends AppCompatActivity {
+public class StartSpeechActivity extends Activity {
 
-    private static MediaRecorder mediaRecorder;
-    private static MediaPlayer mediaPlayer;
+    public static final int SAMPLE_RATE = 16000;
 
-    private static String audioFilePath;
-    private static Button stopButton;
-    private static Button playButton;
-    private static Button recordButton;
-
-    private boolean isRecording = false;
+    private AudioRecord mRecorder;
+    private File mRecording;
+    private short[] mBuffer;
+    private final String startRecordingLabel = "Start recording";
+    private final String stopRecordingLabel = "Stop recording";
+    private boolean mIsRecording = false;
+    private ProgressBar mProgressBar;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_speech);
 
-        recordButton = (Button) findViewById(R.id.recordButton);
-        playButton = (Button) findViewById(R.id.playButton);
-        stopButton = (Button) findViewById(R.id.stopButton);
+        initRecorder();
 
-        if (!hasMicrophone()) {
-            stopButton.setEnabled(false);
-            playButton.setEnabled(false);
-            recordButton.setEnabled(false);
-        } else {
-            playButton.setEnabled(false);
-            stopButton.setEnabled(false);
-        }
+        mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
 
-        audioFilePath =
-                Environment.getExternalStorageDirectory().getAbsolutePath()
-                        + "/myaudio.3gp";
+        final Button button = (Button) findViewById(R.id.button);
+        button.setText(startRecordingLabel);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                if (!mIsRecording) {
+                    button.setText(stopRecordingLabel);
+                    mIsRecording = true;
+                    mRecorder.startRecording();
+                    mRecording = getFile("raw");
+                    startBufferedWrite(mRecording);
+                }
+                else {
+                    button.setText(startRecordingLabel);
+                    mIsRecording = false;
+                    mRecorder.stop();
+                    File waveFile = getFile("wav");
+                    try {
+                        rawToWave(mRecording, waveFile);
+                    } catch (IOException e) {
+                        Toast.makeText(StartSpeechActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                    Toast.makeText(StartSpeechActivity.this, "Recorded to " + waveFile.getName(),
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
-    protected boolean hasMicrophone() {
-        PackageManager pmanager = this.getPackageManager();
-        return pmanager.hasSystemFeature(
-                PackageManager.FEATURE_MICROPHONE);
+    @Override
+    public void onDestroy() {
+        mRecorder.release();
+        super.onDestroy();
     }
 
-    public void recordAudio (View view) throws IOException {
-        isRecording = true;
-        stopButton.setEnabled(true);
-        playButton.setEnabled(false);
-        recordButton.setEnabled(false);
+    private void initRecorder() {
+        int bufferSize = AudioRecord.getMinBufferSize(SAMPLE_RATE, AudioFormat.CHANNEL_IN_MONO,
+                AudioFormat.ENCODING_PCM_16BIT);
+        mBuffer = new short[bufferSize];
+        mRecorder = new AudioRecord(MediaRecorder.AudioSource.MIC, SAMPLE_RATE, AudioFormat.CHANNEL_IN_MONO,
+                AudioFormat.ENCODING_PCM_16BIT, bufferSize);
+    }
 
+    private void startBufferedWrite(final File file) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                DataOutputStream output = null;
+                try {
+                    output = new DataOutputStream(new BufferedOutputStream(new FileOutputStream(file)));
+                    while (mIsRecording) {
+                        double sum = 0;
+                        int readSize = mRecorder.read(mBuffer, 0, mBuffer.length);
+                        for (int i = 0; i < readSize; i++) {
+                            output.writeShort(mBuffer[i]);
+                            sum += mBuffer[i] * mBuffer[i];
+                        }
+                        if (readSize > 0) {
+                            final double amplitude = sum / readSize;
+                            mProgressBar.setProgress((int) Math.sqrt(amplitude));
+                        }
+                    }
+                } catch (IOException e) {
+                    Toast.makeText(StartSpeechActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                } finally {
+                    mProgressBar.setProgress(0);
+                    if (output != null) {
+                        try {
+                            output.flush();
+                        } catch (IOException e) {
+                            Toast.makeText(StartSpeechActivity.this, e.getMessage(), Toast.LENGTH_SHORT)
+                                    .show();
+                        } finally {
+                            try {
+                                output.close();
+                            } catch (IOException e) {
+                                Toast.makeText(StartSpeechActivity.this, e.getMessage(), Toast.LENGTH_SHORT)
+                                        .show();
+                            }
+                        }
+                    }
+                }
+            }
+        }).start();
+    }
+
+    private void rawToWave(final File rawFile, final File waveFile) throws IOException {
+
+        byte[] rawData = new byte[(int) rawFile.length()];
+        DataInputStream input = null;
         try {
-            mediaRecorder = new MediaRecorder();
-            mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-            mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-            mediaRecorder.setOutputFile(audioFilePath);
-            mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
-            mediaRecorder.prepare();
-        } catch (Exception e) {
-            e.printStackTrace();
+            input = new DataInputStream(new FileInputStream(rawFile));
+            input.read(rawData);
+        } finally {
+            if (input != null) {
+                input.close();
+            }
         }
 
-        mediaRecorder.start();
-    }
-
-    public void stopAudio (View view) {
-        stopButton.setEnabled(false);
-        playButton.setEnabled(true);
-
-        if (isRecording) {
-            recordButton.setEnabled(false);
-            mediaRecorder.stop();
-            mediaRecorder.release();
-            mediaRecorder = null;
-            isRecording = false;
-        } else {
-            mediaPlayer.release();
-            mediaPlayer = null;
-            recordButton.setEnabled(true);
+        DataOutputStream output = null;
+        try {
+            output = new DataOutputStream(new FileOutputStream(waveFile));
+            // WAVE header
+            // see http://ccrma.stanford.edu/courses/422/projects/WaveFormat/
+            writeString(output, "RIFF"); // chunk id
+            writeInt(output, 36 + rawData.length); // chunk size
+            writeString(output, "WAVE"); // format
+            writeString(output, "fmt "); // subchunk 1 id
+            writeInt(output, 16); // subchunk 1 size
+            writeShort(output, (short) 1); // audio format (1 = PCM)
+            writeShort(output, (short) 1); // number of channels
+            writeInt(output, SAMPLE_RATE); // sample rate
+            writeInt(output, SAMPLE_RATE * 2); // byte rate
+            writeShort(output, (short) 2); // block align
+            writeShort(output, (short) 16); // bits per sample
+            writeString(output, "data"); // subchunk 2 id
+            writeInt(output, rawData.length); // subchunk 2 size
+            // Audio data (conversion big endian -> little endian)
+            short[] shorts = new short[rawData.length / 2];
+            ByteBuffer.wrap(rawData).order(ByteOrder.LITTLE_ENDIAN).asShortBuffer().get(shorts);
+            ByteBuffer bytes = ByteBuffer.allocate(shorts.length * 2);
+            for (short s : shorts) {
+                bytes.putShort(s);
+            }
+            output.write(bytes.array());
+        } finally {
+            if (output != null) {
+                output.close();
+            }
         }
     }
 
-    public void playAudio (View view) throws IOException {
-        playButton.setEnabled(false);
-        recordButton.setEnabled(false);
-        stopButton.setEnabled(true);
+    private File getFile(final String suffix) {
+        Time time = new Time();
+        time.setToNow();
+        return new File(Environment.getExternalStorageDirectory(), time.format("%Y%m%d%H%M%S") + "." + suffix);
+    }
 
-        mediaPlayer = new MediaPlayer();
-        mediaPlayer.setDataSource(audioFilePath);
-        mediaPlayer.prepare();
-        mediaPlayer.start();
+    private void writeInt(final DataOutputStream output, final int value) throws IOException {
+        output.write(value >> 0);
+        output.write(value >> 8);
+        output.write(value >> 16);
+        output.write(value >> 24);
+    }
+
+    private void writeShort(final DataOutputStream output, final short value) throws IOException {
+        output.write(value >> 0);
+        output.write(value >> 8);
+    }
+
+    private void writeString(final DataOutputStream output, final String value) throws IOException {
+        for (int i = 0; i < value.length(); i++) {
+            output.write(value.charAt(i));
+        }
     }
 }
